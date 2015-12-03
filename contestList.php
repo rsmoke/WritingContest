101dd13bf330eab0eb876a730161a6f944e39eb1
Rick Smoke <rsmoke@umich.edu>
2015-12-03 09:17:38 -0500

    clean up UI

--- contestList.php
+++ contestList.php
@@ -28,9 +28,11 @@ while ($row = $res->fetch_assoc()) {
     }
 }
 
+//    $sql = "SELECT * FROM vw_contestlisting";
+//limit contestlisting to currently open constest 120215
 
-    echo $current_timestamp = date('Y-m-d G:i:s');
     $sql = "SELECT * FROM vw_contestlisting WHERE date_closed > '$current_timestamp'";
+
     $res = $db->query($sql);
 if ($res->num_rows > 0) {
         echo "<table class='table table-responsive table-condensed table-striped'>";
@@ -39,10 +41,12 @@ if ($res->num_rows > 0) {
            // if the contest is available to applicants classlevel the database fieldname will be set to 1 (true) and
            //  this test will be true and the contest will be displayed.
         if ($row["$eligibility"]) {
-                echo'<tr><td><button type="button" data-contest-num="' . $row["contestid"] .
-                  '" class="btn btn-xs btn-success applyBtn"><span class="glyphicon glyphicon-pencil"></span></button></td><td><strong>' .
+                echo'<tr><td><strong>' .
                   $row["ContestsName"] . '</strong><br /><span class="notes">' .
-                  $row["contests_notes"] . '</span></td></tr>';
+                  $row["contests_notes"] . '</span></td>
+                  <td><button type="button" data-contest-num="' . $row["contestid"] .
+                  '" class="btn btn-xs btn-success applyBtn"><span class="glyphicon glyphicon-pencil"></span></button></td>
+                  </tr>';
         }
     }
         echo "</tbody></table>";

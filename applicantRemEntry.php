54611b460a93a3d9dc2fb012074ada626a874032
Rick Smoke <rsmoke@umich.edu>
2016-06-13 15:21:07 -0400

    Add edited_on value

--- applicantRemEntry.php
+++ applicantRemEntry.php
@@ -3,10 +3,10 @@
 require_once($_SERVER["DOCUMENT_ROOT"] . '/../Support/configEnglishContest.php');
 require_once($_SERVER["DOCUMENT_ROOT"] . '/../Support/basicLib.php');
 $entryid = $db->real_escape_string(htmlspecialchars($_POST['sbmid']));
-
+$nowDate = date("Y-m-d H:i:s", (strtotime("now")));
 $sqlUpdate = <<<SQL
     UPDATE tbl_entry
-    SET status = 1, edited_by = '$login_name'
+    SET status = 1, edited_by = '$login_name', edited_on = $nowDate
     WHERE id = $entryid
 SQL;
     if(!$result = $db->query($sqlUpdate)){

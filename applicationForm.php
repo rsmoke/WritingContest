76a2e1d90b160e6280a0901231e1be8c67b78c3c
Rick Smoke <rsmoke@umich.edu>
2016-06-14 08:59:29 -0400

    Update verbage on cover page details

--- applicationForm.php
+++ applicationForm.php
@@ -337,7 +337,7 @@ SQL;
             <label class="control-label" for="fileToUpload">Select file to upload (it must be in PDF format):</label>
             <input type="hidden" name="MAX_FILE_SIZE" value="20971520" />
             <input type="file" name="fileToUpload" id="fileToUpload" required />
-            <span id="helpBlock2" class="help-block">Title (or first) page of pdf should be <strong><em>title of your manuscript, pen-name</em></strong> and <strong><em>area</em></strong> only.</span>
+            <span id="helpBlock2" class="help-block">Title (or first) page of pdf should be <strong><em>title of your manuscript, pen-name</em></strong> and <strong><em>contest and division entered (e.g., Summer Hopwood Poetry)</em></strong> only.</span>
           </div>
           <div class='text-center'>
           <input class="btn btn-success" type="submit" name="upload" value="Upload Application">

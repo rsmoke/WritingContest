0e98470ee2ad51818ea30ea1182f13f5b4812f8c
Rick Smoke <rsmoke@umich.edu>
2017-02-06 12:33:46 -0500

    Add manuscript to submitted list

--- individualSubmission.php
+++ individualSubmission.php
@@ -3,23 +3,24 @@
 require_once($_SERVER["DOCUMENT_ROOT"] . '/../Support/configEnglishContest.php');
 require_once($_SERVER["DOCUMENT_ROOT"] . '/../Support/basicLib.php');
 
-    $sql = "SELECT * FROM vw_entrydetail WHERE uniqname = '$login_name' AND status = 0";
-    if(!$result = $db->query($sql)){
-        db_fatal_error("Individual submission" , $db->error, $sql, $login_name);
-        exit($user_err_message);
-    }
+    $sql = "SELECT contestName, manuscriptType, document, title, datesubmitted, EntryId FROM vw_entrydetail WHERE uniqname = '$login_name' AND status = 0";
+    if ($result = $db->prepare($sql)) {
+        $result->execute();
+        $result->bind_result($contestName, $manuscriptType, $document, $title,  $datesubmitted,  $EntryId);
+        $result->store_result();
 
-    if ($result->num_rows > 0) {
         echo "<table class='table table-responsive table-condensed'>";
-        echo "<thead><th>Contest</th><th>Entry Type</th><th>Title</th><th>Submitted</th><th class='btnIcon'>Details</th><th class='btnIcon'>Remove</th></thead><tbody>";
-        while ($row = $result->fetch_assoc()) {
+        echo "<thead><th>Contest</th><th>Entry Type</th><th>Title</th><th>Submitted</th><th class='btnIcon'>Manuscript<br><span style='font-size:.9rem;'>(opens in a new browser tab)</span></th><th class='btnIcon'>Remove</th></thead><tbody>";
+        for ($i = 0; $i < $result->num_rows; $i++) {
+          $result->data_seek($i);
+          $result->fetch();
             echo "<tr><td>";
-            echo $row["contestName"] . "</td><td>";
-            echo $row["manuscriptType"] . "</td><td>";
-            echo $row["title"] . "</td><td>";
-            echo date("F jS, Y  g:i A", (strtotime($row["datesubmitted"]))) . "</td>";
-            echo "<td class='btnIcon'><button class='btn btn-warning btn-xs covshtbtn' data-entryid='" . $row["EntryId"] . "'><span class='glyphicon glyphicon-file'></span></button></td>";
-            echo "<td class='btnIcon'><button class='btn btn-danger btn-xs applicantdeletebtn' data-entryid='" . $row["EntryId"] . "'><span class='glyphicon glyphicon-remove-sign'></span></button></td></tr>";
+            echo "$contestName</td><td>";
+            echo "$manuscriptType</td><td>";
+            echo "$title</td><td>";
+            echo date("F jS, Y  g:i A", (strtotime($datesubmitted))) . "</td>";
+            echo "<td class='btnIcon'><a href='fileholder.php?file=$document' target='_blank'><span class='glyphicon glyphicon-book'></span></a></td>";
+            echo "<td class='btnIcon'><button class='btn btn-danger btn-xs applicantdeletebtn' data-entryid='$EntryId'><span class='glyphicon glyphicon-remove-sign'></span></button></td></tr>";
         }
           echo "</tbody></table>";
     } else {
